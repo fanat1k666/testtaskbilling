a09ddbc3751b074d42e404f1cb373392e842c223
shogu <shogura666@gmail.com>
2024-01-22 13:02:12 +0300

    finishx2

--- db/migrations/20240121093012_tariffs.sql
+++ db/migrations/20240121093012_tariffs.sql
@@ -5,6 +5,7 @@ CREATE TABLE tariffs
     id   serial primary key,
     name varchar(255) NOT NULL,
     price int NOT NULL,
+    speed varchar(255),
     CONSTRAINT tariffs_unique UNIQUE (name)
 );
 -- +goose StatementEnd
@@ -24,6 +25,7 @@ CREATE TABLE user_tariffs
     id             serial primary key,
     user_id        int       NOT NULL,
     tariffs_id     int       NOT NULL,
+    address varchar(255),
     constraint user_fk foreign key (user_id) references "user" on update cascade on delete cascade,
     constraint tariffs_fk foreign key (tariffs_id) references tariffs on update cascade on delete cascade
 );
